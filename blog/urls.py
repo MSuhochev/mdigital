14b8eb59a227d9a98584045cefb88da6f70d483c
Maxim Mikhailovich <suhochev_m@mail.ru>
2024-04-19 20:20:02 +0300

    added PostGrid - with pagination

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.HomeView.as_view(), name='home'),
+    path('post_grid/', views.PostGridView.as_view(), name='post_grid'),
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_single'),
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    slug_url_kwarg = 'post_slug'
 
 
 class PostListView(ListView):
@@ -10,10 +22,26 @@ class PostListView(ListView):
         return Post.objects.select_related('category').filter(category__slug=self.kwargs.get('slug'))
 
 
-class PostDetailView(DetailView):
+class PostGridView(ListView):
     model = Post
-    context_object_name = 'post'
-    slug_url_kwarg = 'post_slug'
+    paginate_by = 4
+    template_name = "blog/post_grid.html"
+    context_object_name = 'posts'
+    ordering = ['-create_at']
 
-def home(request):
-    return render(request, "index.html")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts_list = Post.objects.all().order_by('-create_at')
+        paginator = Paginator(posts_list, self.paginate_by)
+        page = self.request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        context['posts'] = posts
+        return context
+
+    def get_queryset(self):
+        return Post.objects.order_by('-create_at')
