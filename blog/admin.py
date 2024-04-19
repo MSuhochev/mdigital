87f71f969a7fb773e2502d3d393f786accba9c35
Maxim Mikhailovich <suhochev_m@mail.ru>
2024-04-19 13:46:46 +0300

    added CKEditor_5 in PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,26 @@
+from django import forms
 from django.contrib import admin
+from django_ckeditor_5.widgets import CKEditor5Widget
 from mptt.admin import MPTTModelAdmin
 from . import models
+from .models import Post
+
+
+class PostAdminForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditor5Widget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        widgets = {
+            "text": CKEditor5Widget(
+                attrs={"class": "django_ckeditor_5"}, config_name='extends'
+            )
+        }
 
 
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     list_display = ['title', 'category', 'author', 'create_at', 'id']
 
 
--- mdigital/urls.py
+++ mdigital/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
     path('', include('blog.urls')),
 ]
 
