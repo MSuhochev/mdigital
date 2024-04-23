bf7ee926a09920b3f64ac468a0d1d1d0ba21c4d4
Maxim Mikhailovich <suhochev_m@mail.ru>
2024-04-23 10:34:16 +0300

    edit index-page 99%

--- blog/templatetags/menu_tags.py
+++ blog/templatetags/menu_tags.py
@@ -4,7 +4,7 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag('blog/include/tags/top_menu.html')
+@register.inclusion_tag('blog/includes/tags/top_menu.html')
 def get_categories():
     category = Category.objects.filter(parent__isnull=True).order_by('id')
     return {'list_category': category}
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
 
